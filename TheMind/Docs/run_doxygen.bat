127c3bcbcc3cca6c8d74b1772101653615735809
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2021-12-01 22:26:00 +0100

    Doxygen Shared

--- TheMind/Docs/run_doxygen.bat
+++ TheMind/Docs/run_doxygen.bat
@@ -4,9 +4,11 @@
 	del /s /f /q ServerProject
 	del /s /f /q ClientProject
 	del /s /f /q BotProject
+	del /s /f /q Shared
 	doxygen Doxyfile_ServerProject
 	doxygen Doxyfile_ClientProject
 	doxygen Doxyfile_BotProject
+	doxygen Doxyfile_Shared
 	echo Appuyez sur une touche pour reg‚n‚rer la documentation...
 	pause >NUL
 goto loop
